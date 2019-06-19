a7f0bba85680fb3141abd74d3907accdbd82f31a
Channelwood <channelwood@protonmail.com>
2019-06-18 22:37:15 -0400

    commit changes to run

--- run.m
+++ run.m
@@ -1,6 +1,6 @@
 % set globals
 dur_min = .5;
-dur_sec = 60 * dur_sec;
+dur_sec = 60 * dur_min;
 
 % useful constants
 acc_sr = 32; % hertz
@@ -8,9 +8,16 @@ eda_sr = 4; % hertz
 hr_sr = 1; % hertz
 temp_sr = 4; % hertz 
 
+% generate data groups
+split
+
 
 % proc_acc
 % proc_eda
 % proc_hr
 % proc_ibi
 % proc_temp
+
+% clean up workspace
+vars = {"dur_min", "dur_sec", "acc_sr", "eda_sr", "hr_sr", "temp_sr", "vars"};
+clear(vars{:});
